3964f538a151f870fedfc23fd0253ca8108a1f1c
Buertey Accam <111708279+george-accam@users.noreply.github.com>
2026-02-07 17:43:56 +0000

    Add routes and controller methods for retrieving organiser and all donation balances; enhance donation processing logic

--- components/controllers/DonationController.js
+++ components/controllers/DonationController.js
@@ -1,103 +1,149 @@
 const api = require("../axios/api");
 const FuneralDetails = require("../models/FuneralDetailsModel");
 const Donation = require("../models/DonationModel");
+const OrganiserDonationBalance = require("../models/organiserDonationBalanceModel");
 const Guest = require("../models/GuestModel");
 
 // Donate to event
 exports.makeDonation = async (req, res) => {
-    try {
-        const { funeralUniqueCode, guestId, donationAmount } = req.body;
+  try {
+    const { funeralUniqueCode, guestId, donationAmount } = req.body;
 
-        if(!funeralUniqueCode || !guestId || !donationAmount){
-            return res.status(404).json({
-                success: false,
-                message: "All fields are required"
-            });
-        };
-
-        // Verify guest exists
-        const guest = await Guest.findById(guestId);
-        if (!guest) {
-            return res.status(404).json({ message: "Guest not found" });
-        }
-
-        const funeralDetails = await FuneralDetails.findOne({ uniqueCode: funeralUniqueCode, donation: true });
-        if (!funeralDetails) {
-            return res.status(404).json({ 
-                success: false,
-                message: "This event does not accept donations", 
-                donationAllowed: false
-            });
-        }
-        // Initialize transaction with Paystack
-        const response = await api.post("transaction/initialize", {
-          email: "huskpaystackreceipt@gmail.com",
-          amount: donationAmount * 100, // Paystack expects amount in kobo/old pesewas
-          metadata: {
-            funeralUniqueCode,
-            guestId,
-          },
-        });
-
-        // respond with authorization URL and reference
-        const { authorization_url, reference } = response.data.data;
-
-        res.status(200).json({ 
-            success: true,
-            message: "Donation initialized successfully",
-            reference: reference,
-            url: authorization_url, 
-         });
-    } catch (error) {
-        console.error("Error making donation:", error);
-        res.status(500).json({ 
-            success: false,
-            message: `Server error: ${error.message}` 
-        });
+    if (!funeralUniqueCode || !guestId || !donationAmount) {
+      return res.status(404).json({
+        success: false,
+        message: "All fields are required",
+      });
     }
+
+    // Verify guest exists
+    const guest = await Guest.findById(guestId);
+    if (!guest) {
+      return res.status(404).json({ message: "Guest not found" });
+    }
+
+    const funeralDetails = await FuneralDetails.findOne({
+      uniqueCode: funeralUniqueCode,
+      donation: true,
+    });
+    if (!funeralDetails) {
+      return res.status(404).json({
+        success: false,
+        message: "This event does not accept donations",
+        donationAllowed: false,
+      });
+    }
+    // Initialize transaction with Paystack
+    const response = await api.post("transaction/initialize", {
+      email: "huskpaystackreceipt@gmail.com",
+      amount: donationAmount * 100, // Paystack expects amount in kobo/old pesewas
+      metadata: {
+        funeralUniqueCode,
+        guestId,
+      },
+    });
+
+    // respond with authorization URL and reference
+    const { authorization_url, reference } = response.data.data;
+
+    res.status(200).json({
+      success: true,
+      message: "Donation initialized successfully",
+      reference: reference,
+      url: authorization_url,
+    });
+  } catch (error) {
+    console.error("Error making donation:", error);
+    res.status(500).json({
+      success: false,
+      message: `Server error: ${error.message}`,
+    });
+  }
 };
 
 // Get donation by event unique code
 exports.getDonationsByEvent = async (req, res) => {
-    try {
-        const { funeralUniqueCode } = req.params;
-        const donations = await Donation.find({ funeralUniqueCode }).populate("guestId", "fullName phoneNumber");
-        if (!donations) {
-            return res.status(404).json({
-                success: false,
-                message: "No donations found for this event"
-            });
-        };
+  try {
+    const { funeralUniqueCode } = req.params;
+    const donations = await Donation.find({ funeralUniqueCode }).populate(
+      "guestId",
+      "fullName phoneNumber",
+    );
+    if (!donations) {
+      return res.status(404).json({
+        success: false,
+        message: "No donations found for this event",
+      });
+    }
 
-        res.status(200).json({
-            success: true,
-            message: "Donations retrieved successfully",
-            donations 
-        });
-    }
-    catch (error) {
-        console.error("Error retrieving donations:", error);
-        res.status(500).json({ 
-            success: false,
-            message: `Server error: ${error.message}` 
-        });
-    }
+    res.status(200).json({
+      success: true,
+      message: "Donations retrieved successfully",
+      donations,
+    });
+  } catch (error) {
+    console.error("Error retrieving donations:", error);
+    res.status(500).json({
+      success: false,
+      message: `Server error: ${error.message}`,
+    });
+  }
 };
 
-// Get all donations
+// Get all donations (admin)
 exports.getAllDonations = async (req, res) => {
-    try {
-        const donations = await Donation.find().populate("guestId", "name email");
-        res.status(200).json({
-            success: true,
-            message: "Donations retrieved successfully",
-            donations 
-        });
-    } catch (error) {
-        console.error("Error retrieving donations:", error);
-        res.status(500).json({ 
-            success: false,
-            message: `Server error: ${error.message}`
-        });
-    }
+  try {
+    const donations = await Donation.find().populate("guestId", "name email");
+    res.status(200).json({
+      success: true,
+      message: "Donations retrieved successfully",
+      donations,
+    });
+  } catch (error) {
+    console.error("Error retrieving donations:", error);
+    res.status(500).json({
+      success: false,
+      message: `Server error: ${error.message}`,
+    });
+  }
+};
+
+// get organizer donation balance(organizer)
+exports.getOrganiserDonationBalance = async (req, res) => {
+  try {
+    const { organiserId } = req.params;
+    const donationBalance = await OrganiserDonationBalance.findOne({
+      organiserId,
+    });
+    res.status(200).json({
+      success: true,
+      message: "Balance retrieved successfully",
+      balance: donationBalance,
+    });
+  } catch (error) {
+    console.error("Error retrieving donations:", error);
+    res.status(500).json({
+      success: false,
+      message: `Server error: ${error.message}`,
+    });
+  }
+};
+
+// get all donation balance (admin)
+exports.getAllDonationBalance = async (req, res) => {
+  try {
+    const donationBalance = await OrganiserDonationBalance.find().populate("organiserId", "firstName lastName phoneNumber");
+    res.status(200).json({
+      success: true,
+      message: "Balance retrieved successfully",
+      balance: donationBalance,
+    });
+
+  } catch (error) {
+    console.error("Error retrieving donations:", error);
+    res.status(500).json({
+      success: false,
+      message: `Server error: ${error.message}`,
+    });
+  }
 };
--- components/routers/DonationRoute.js
+++ components/routers/DonationRoute.js
@@ -1,4 +1,4 @@
-const { makeDonation, getDonationsByEvent, getAllDonations } = require("../controllers/DonationController");
+const { makeDonation, getDonationsByEvent, getAllDonations, getAllDonationBalance, getOrganiserDonationBalance } = require("../controllers/DonationController");
 const { verifyToken } = require("../middleware/Authenticate");
 const express = require("express");
 const router = express.Router();
@@ -6,5 +6,7 @@ const router = express.Router();
 router.post("/make-donation", verifyToken, makeDonation);
 router.get("/fetch-donation/:funeralUniqueCode", verifyToken, getDonationsByEvent);
 router.get("/fetch-donations", verifyToken, getAllDonations);
+router.get("/fetch-organiser-balance/:organiserId", verifyToken, getOrganiserDonationBalance);
+router.get("/fetch-donation-balance", verifyToken, getAllDonationBalance);
 
 module.exports = router;
