6a69defc391486e8a8db20b3a22f9b8ef828bb37
Buertey Accam <111708279+george-accam@users.noreply.github.com>
2026-02-08 13:40:25 +0000

    Refactor recipient verification update logic; improve code clarity and logging

--- components/reminders/recipientVerifyUpdate.js
+++ components/reminders/recipientVerifyUpdate.js
@@ -3,13 +3,26 @@ const TransferRecipient = require("../models/TransferRecipientModel");
 
 const updateRecipientVerificationCodes = async () => {
     try {
+        const now = new Date(); // current date
+        //calculate 30 minutes ago
+        const thirtyMinutesAgo = (now - 30 * 60 * 1000);
+
+        // update transfer recipients whose verification is true 
+        const transferRecipient = await TransferRecipient.updateMany({
+            isVerified: true,
+            updatedAt: { $lte: thirtyMinutesAgo }
+        }, 
+        {
+            $set: { isVerified: false }
+        });
+        console.log(`Number of recipient codes updated: ${transferRecipient.modifiedCount}`);
         
-        console.log("I'm working");
+        console.log(`Checking for expired verification codes`);
         
     } catch (error) {
         throw new Error(`Error occurred updating verification codes: ${error.message}`)
     }
 };
 
-cron.schedule("*/30 * * * *", updateRecipientVerificationCodes);
+cron.schedule("* * * * *", updateRecipientVerificationCodes);
 module.exports = updateRecipientVerificationCodes;
